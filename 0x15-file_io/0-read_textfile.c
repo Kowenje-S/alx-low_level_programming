d18447ecf8d8ba30385119f999e9876d52bd18b3
Kowenje-S <kowenjesandra@gmail.com>
2023-09-04 01:10:03 -0700

    updated

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - reads a text file
+ * @filename: ...
+ * @letters: ...
+ *
+ * Return: ..
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	let = read(file, text, letters);
+	w = write(STDOUT_FILENO, text, let);
+	close(file);
+	return (w);
+}
